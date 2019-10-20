5ed6fcfb7f3195248b81464a8f868107a6835a7d
HaiboZhu <hbzhu@thoughtworks.com>
2019-10-20 17:57:22 +0800

    add some dependencies and bootstrap ref.

---
+++ src/main/java/com/myschool/api/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.myschool.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomeController {
+
+    @RequestMapping("/welcome")
+    public String loginMessage(){
+        return "welcome";
+    }
+}
